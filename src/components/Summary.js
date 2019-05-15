b25eb0960a5b3be41bfd7b74f84af3bd47ff8e9e
ross-u <cirkovic.uros.srb@gmail.com>
2019-05-15 22:48:07 +0200

    Initial commit

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,11 @@
 import React from 'react'
 
 function summary(props) {
-  
-  let tasksCompleted = 0;
-  props.tasks.forEach((task) => {
-    if(task.isDone) tasksCompleted++;
-  });
 
   return (
     <div>
       <h1>TASKS COMPLETED:</h1>
-      <p className="tasks-completed">{tasksCompleted}</p>
+      <p className="tasks-completed">{props.tasksCompleted}</p>
     </div>
   )
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -7,11 +7,12 @@ class ToDoList extends Component {
   constructor() {
     super();
     this.state = {
-      tasks: data
+      tasks: data,
+      tasksCompleted: 0
     };
     
     this.toggleTaskDone = this.toggleTaskDone.bind(this);
-    this.deleteToDoList = this.deleteToDoList.bind(this);   
+    this.deleteTaskById = this.deleteTaskById.bind(this);   
     /*** 
       We bind the method because it will be called by a component (<Task> - object)
       and without binding, `this` will be pointing to `<Task>` component that invoked the method.
@@ -20,14 +21,17 @@ class ToDoList extends Component {
   };
   
   toggleTaskDone(id) {
-    console.log('What is `this`', this)
-    const taskListCopy = [...this.state.tasks];
-    taskListCopy.forEach((taskObj) => {
-      if(taskObj.id === id) {
-        taskObj.isDone = !taskObj.isDone;
+    const tasksCopy = [...this.state.tasks];
+    let tasksCompleted = this.state.tasksCompleted;
+
+    tasksCopy.forEach((oneTask) => {
+      if(oneTask.id === id) {
+        oneTask.isDone = oneTask.isDone ? false : true;
+        if (oneTask.isDone) tasksCompleted++;
+        else if (!oneTask.isDone) tasksCompleted--;        
       }
     })
-    this.setState( {tasks: taskListCopy} );
+    this.setState( {tasks: tasksCopy, tasksCompleted } );
   }
 
   /***  
@@ -35,32 +39,33 @@ class ToDoList extends Component {
     Splice modifies the orginal array it's called on. 
     Therefore we're using the spread operator to copy the array first. ;)
   ***/
-  deleteToDoList (id)  {
+  deleteTaskById (id)  {
     const taskListCopy = [...this.state.tasks];
+    let tasksCompleted = this.state.tasksCompleted;
+
     taskListCopy.forEach((taskObj, index) => {
       if(taskObj.id === id) {
         taskListCopy.splice(index, 1);
+        tasksCompleted--;
       }
     })
-    this.setState( {tasks: taskListCopy} );
+    this.setState( {tasks: taskListCopy, tasksCompleted} );
   };
 
   render() {
     return (
       <div>
-        <Summary tasks={this.state.tasks}/>
+        <Summary tasksCompleted={this.state.tasksCompleted}/>
         <div className='todo-container'>
-          {
-            this.state.tasks.map( (task) => {
-              return <Task 
-                      id={task.id}
-                      deleteTask={ this.deleteToDoList }
-                      updateTaskStatus={ this.toggleTaskDone }
-                      key={task.id} 
-                      {...task}
-                    />
-            })
-          }
+        {
+          this.state.tasks.map( (task) => {
+            return <Task key={task.id} 
+                    deleteTask={ this.deleteTaskById }
+                    updateTaskStatus={ this.toggleTaskDone }
+                    {...task}
+                  />
+          })
+        }
         </div>
       </div>
     )
