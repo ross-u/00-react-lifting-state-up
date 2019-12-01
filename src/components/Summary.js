94765153cda62fda14817e848ff02c2c86e73921
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-12-01 21:34:16 +0100

    Update comments and function constructor name

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-function summary(props) {
-
+function Summary(props) {
   return (
     <div>
       <h1>TASKS COMPLETED:</h1>
       <p className="tasks-completed">0</p>
     </div>
-  )
+  );
 }
 
-export default summary;
+export default Summary;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Task from "./Task";
+import Task from './Task';
 import Summary from './Summary';
-import data from './../data.js'
+import data from './../data.js';
 
 class ToDoList extends Component {
   constructor() {
@@ -9,50 +9,46 @@ class ToDoList extends Component {
     this.state = {
       tasks: data,
     };
-    
-    this.deleteTaskById = this.deleteTaskById.bind(this);   
+
+    this.deleteTaskById = this.deleteTaskById.bind(this);
     /*** 
-      We bind the method because it will be called by a component (<Task> - object)
-      and without binding, `this` will be pointing to `<Task>` component that invoked the method.
-      We `bind` `this` as we need it to point to <ToDoList> component which has the `state`.
+      We bind the method because it will be invoked by another component (<Task> - object)
+      Without binding, `this` will be refering to `<Task>` component that invoked the method.
+      We `bind` `this` as we need it to point to <ToDoList> component (object) that holds the `state`.
     ***/
-  };
+  }
 
   /***  
-    State shouldn't be modified directly. 
-    Splice modifies the orginal array it's called on. 
+    1. State shouldn't be modified directly and `splice()` modifies the orginal array it's called on. 
     Therefore we're using the spread operator to copy the array first. ;)
   ***/
-  deleteTaskById (id)  {
+  deleteTaskById(id) {
     const taskListCopy = [...this.state.tasks];
     let tasksCompleted = this.state.tasksCompleted;
 
     taskListCopy.forEach((taskObj, index) => {
-      if(taskObj.id === id) {
+      if (taskObj.id === id) {
         taskListCopy.splice(index, 1);
         tasksCompleted--;
       }
-    })
-    this.setState( {tasks: taskListCopy, tasksCompleted} );
-  };
+    });
+    this.setState({ tasks: taskListCopy, tasksCompleted });
+  }
 
   render() {
     return (
       <div>
         <Summary />
-        <div className='todo-container'>
-        {
-          this.state.tasks.map( (task) => {
-            return <Task key={task.id} 
-                    deleteTask={ this.deleteTaskById }
-                    {...task}
-                  />
-          })
-        }
+        <div className="todo-container">
+          {this.state.tasks.map(task => {
+            return (
+              <Task key={task.id} deleteTask={this.deleteTaskById} {...task} />
+            );
+          })}
         </div>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default ToDoList;
