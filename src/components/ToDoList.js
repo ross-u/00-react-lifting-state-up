71e37f28487b482080a8a767cb3e47d1feb179d4
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-26 11:38:29 +0100

    Lecture code example

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -6,11 +6,14 @@ import data from "./../data.js";
 class ToDoList extends Component {
   constructor() {
     super();
+
     this.state = {
-      tasks: data
+      tasks: data,
+      tasksCompleted: 0
     };
 
     this.deleteTaskById = this.deleteTaskById.bind(this);
+    this.toggleTaskDone = this.toggleTaskDone.bind(this);
     /*** 
       We bind the method because it will be invoked by another component (<Task> - object)
       Without binding, `this` will be refering to `<Task>` component that invoked the method.
@@ -30,20 +33,51 @@ class ToDoList extends Component {
     taskListCopy.forEach((taskObj, index) => {
       if (taskObj.id === id) {
         taskListCopy.splice(index, 1);
-        tasksCompleted--;
+
+        // tasksCompleted--;
+        if (taskObj.isDone) tasksCompleted--;
       }
     });
     this.setState({ tasks: taskListCopy, tasksCompleted });
   }
 
+  toggleTaskDone(id) {
+    const tasksCopy = [...this.state.tasks];
+    let tasksCompleted = this.state.tasksCompleted;
+    // let {tasksCompleted} = this.state;
+
+    // Find the clicked task
+    // Update the `isDone` property
+    // Update the `tasksCompleted` counter
+    // Set the updated state
+
+    tasksCopy.forEach(taskObj => {
+      if (taskObj.id === id) {
+        taskObj.isDone = !taskObj.isDone;
+        // taskObj.isDone = taskObj.isDone ? false : true;
+
+        if (taskObj.isDone) tasksCompleted++;
+        else if (!taskObj.isDone) tasksCompleted--;
+      }
+    });
+
+    this.setState({ tasks: tasksCopy, tasksCompleted });
+  }
+
   render() {
     return (
       <div>
-        <Summary />
+        <Summary tasksCompleted={this.state.tasksCompleted}/>
+
         <div className="todo-container">
           {this.state.tasks.map(task => {
             return (
-              <Task key={task.id} deleteTask={this.deleteTaskById} {...task} />
+              <Task 
+                key={task.id} 
+                deleteTask={this.deleteTaskById}
+                updateTaskDone={this.toggleTaskDone}
+                {...task}
+              />
             );
           })}
         </div>
