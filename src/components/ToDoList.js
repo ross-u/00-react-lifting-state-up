07cbd12f2c32e5fddf4ecffaaaff3b319dc255f1
ross-u <cirkovic.uros.srb@gmail.com>
2021-02-24 08:51:44 +0100

    Update example

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -7,34 +7,32 @@ class ToDoList extends Component {
   constructor() {
     super();
     this.state = {
-      tasks: data
+      tasks: data,
     };
 
     this.deleteTaskById = this.deleteTaskById.bind(this);
     /*** 
       We bind the method because it will be invoked by another component (<Task> - object)
-      Without binding, `this` will not be refering to the `<ToDoList>` component.
+      Without binding, `this` will not be referring to the `<ToDoList>` component.
       We `bind` `this` as we need it to point to <ToDoList> component (object) that holds the `state`.
       We can as well create the method as an arrow function to solve this issue of losing context.
     ***/
   }
 
   /***  
-    1. State shouldn't be modified directly and `splice()` modifies the orginal array it's called on. 
-    Therefore we're using the spread operator to copy the array first. ;)
-    As an alternative we can also use the `.filter` method.
+    1. State shouldn't be modified directly, therefore we use the `filter` method 
+    which creates a new array
   ***/
   deleteTaskById(id) {
-    const taskListCopy = [...this.state.tasks];
     let tasksCompleted = this.state.tasksCompleted;
 
-    taskListCopy.forEach((taskObj, index) => {
+    const updatedTasks = this.state.tasks.filter((taskObj, index) => {
       if (taskObj.id === id) {
-        taskListCopy.splice(index, 1);
-        tasksCompleted--;
+        return false;
       }
+      return true;
     });
-    this.setState({ tasks: taskListCopy, tasksCompleted });
+    this.setState({ tasks: updatedTasks, tasksCompleted });
   }
 
   render() {
@@ -42,7 +40,7 @@ class ToDoList extends Component {
       <div>
         <Summary />
         <div className="todo-container">
-          {this.state.tasks.map(task => {
+          {this.state.tasks.map((task) => {
             return (
               <Task key={task.id} deleteTask={this.deleteTaskById} {...task} />
             );
